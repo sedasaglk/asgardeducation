3d96db16f3ef428985111b23c7ed4fb8ecb4d6f5
Seda <sofsyazilim@gmail.com>
2021-03-20 18:32:23 +0300

    Django Girls uygulamam, ilk commit

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+
+
+class Post(models.Model):
+    title = models.CharField(max_length= 100)
+    subtitle = models.CharField(max_length= 200)
+    content = RichTextField()
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='image/', null=True)
+
+    class Meta:
+        ordering = ['-date']
+    
+    def __str__(self):
+        return self.title
